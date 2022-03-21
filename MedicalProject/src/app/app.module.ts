10799654047f00c04e99b40cadbb0a237cf2125e
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2022-03-21 17:36:17 +0300

    добавила новые элементы на главную страницу

--- MedicalProject/src/app/app.module.ts
+++ MedicalProject/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
